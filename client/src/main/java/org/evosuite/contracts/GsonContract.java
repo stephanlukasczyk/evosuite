956bcd3a978972af2a3dd107569dd46b1614bae3
Stephan Lukasczyk <stephan@lukasczyk.me>
2018-05-15 15:52:41 +0200

    GsonContract: format code according to Google Code Style

--- client/src/main/java/org/evosuite/contracts/GsonContract.java
+++ client/src/main/java/org/evosuite/contracts/GsonContract.java
@@ -21,6 +21,8 @@ package org.evosuite.contracts;
 
 import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
+import java.lang.reflect.TypeVariable;
+import java.util.List;
 import org.evosuite.Properties;
 import org.evosuite.testcase.execution.Scope;
 import org.evosuite.testcase.statements.Statement;
@@ -28,79 +30,80 @@ import org.evosuite.testcase.variable.VariableReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.TypeVariable;
-import java.util.List;
-
 /**
  * A contract checking for equality of an object before and after serialization with GSON.
  *
- * <p>Each object will be fed to Google's GSON library, serialized to JSON format, and
- * de-serialized back from JSON.  The resulting object should be equal to the initial object.  If
- * this is not the case, we also compare the XML representations, created by XStream; this is
- * because we want to remove false positives that occur only due to an insufficient {@code equals}
- * implementation of some types.
- * </p>
+ * <p>Each object will be fed to Google's GSON library, serialized to JSON format, and de-serialized
+ * back from JSON. The resulting object should be equal to the initial object. If this is not the
+ * case, we also compare the XML representations, created by XStream; this is because we want to
+ * remove false positives that occur only due to an insufficient {@code equals} implementation of
+ * some types.
  *
  * @author Stephan Lukasczyk
  */
 public class GsonContract extends Contract {
 
-    private static Logger logger = LoggerFactory.getLogger(Contract.class);
+  private static final Logger logger = LoggerFactory.getLogger(Contract.class);
 
-    private final Gson gson;
-    private final XStream xstream;
+  private final Gson gson;
+  private final XStream xstream;
 
-    GsonContract() {
-        gson = new Gson();
-        xstream = new XStream();
-    }
+  GsonContract() {
+    gson = new Gson();
+    xstream = new XStream();
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public ContractViolation check(Statement statement, Scope scope, Throwable exception) {
-        for (Object originalObject : scope.getObjects()) {
-            // if object has generic type, ignore it, because GSON cannot handle those properly
-            final TypeVariable<? extends Class<?>>[] typeParameters =
-                    originalObject.getClass().getTypeParameters();
-            if (typeParameters.length > 0) {
-                continue;
-            }
+  /** {@inheritDoc} */
+  @Override
+  public ContractViolation check(
+      final Statement statement, final Scope scope, final Throwable exception) {
+    for (Object originalObject : scope.getObjects()) {
+      // if object has generic type, ignore it, because GSON cannot handle those properly
+      final TypeVariable<? extends Class<?>>[] typeParameters =
+          originalObject.getClass().getTypeParameters();
+      if (typeParameters.length > 0) {
+        continue;
+      }
 
-            if (!originalObject.getClass().getSimpleName().equals(Properties.TARGET_CLASS)) {
-                continue;
-            }
+      if (!originalObject.getClass().getSimpleName().equals(Properties.TARGET_CLASS)) {
+        continue;
+      }
 
-            // skip Object, as it will not be de-serialized correctly
-            if ("Object".equals(originalObject.getClass().getSimpleName())) {
-                continue;
-            }
+      // skip Object, as it will not be de-serialized correctly
+      if ("Object".equals(originalObject.getClass().getSimpleName())) {
+        continue;
+      }
 
-            final String gsonResult = gson.toJson(originalObject);
-            final Object gsonDeserialize = gson.fromJson(gsonResult, originalObject.getClass());
+      final String gsonResult = gson.toJson(originalObject);
+      final Object gsonDeserialize = gson.fromJson(gsonResult, originalObject.getClass());
 
-            if (!originalObject.equals(gsonDeserialize)) {
-                final String oXML = xstream.toXML(originalObject);
-                final String deserializeXML = xstream.toXML(gsonDeserialize);
-                if (!oXML.equals(deserializeXML)) {
-                    logger.debug("Found a contract violation.\n\t\tOrig: " + originalObject
-                            + "\n\t\tGSON: " + gsonDeserialize + "\n\t\tJSON: " + gsonResult);
-                    return new ContractViolation(this, statement, exception);
-                }
-            }
+      if (!originalObject.equals(gsonDeserialize)) {
+        final String oXML = xstream.toXML(originalObject);
+        final String deserializeXML = xstream.toXML(gsonDeserialize);
+        if (!oXML.equals(deserializeXML)) {
+          logger.debug(
+              "Found a contract violation.\n\t\tOrig: "
+                  + originalObject
+                  + "\n\t\tGSON: "
+                  + gsonDeserialize
+                  + "\n\t\tJSON: "
+                  + gsonResult);
+          return new ContractViolation(this, statement, exception);
         }
-        return null;
+      }
     }
+    return null;
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public void addAssertionAndComments(
-            Statement statement, List<VariableReference> variables, Throwable exception) {
-        final String vars =
-                variables != null && variables.size() > 0
-                        ? ", Variables: " + variables.toString() : "";
-        final String ex =
-                exception != null
-                        ? ", Exception: " + exception.getMessage() : "";
-        statement.addComment("GsonContract failed. Statement: " + statement + vars + ex);
-    }
+  /** {@inheritDoc} */
+  @Override
+  public void addAssertionAndComments(
+      final Statement statement,
+      final List<VariableReference> variables,
+      final Throwable exception) {
+    final String vars =
+        variables != null && variables.size() > 0 ? ", Variables: " + variables.toString() : "";
+    final String ex = exception != null ? ", Exception: " + exception.getMessage() : "";
+    statement.addComment("GsonContract failed. Statement: " + statement + vars + ex);
+  }
 }
